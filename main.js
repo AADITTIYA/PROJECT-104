273d36349285f632a7c8fd6a59e1b6e9d1829bb9
AADITTIYA <80966760+AADITTIYA@users.noreply.github.com>
2022-03-16 19:27:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    height:300,
+    width:350,
+    image_format: "png",
+    png_quality:90
+});
+
+Webcam.attach("#live_camera")
+ 
+
+function  take_snap(){
+    Webcam.snap(function  (data_uri){
+        document.getElementById("snap").innerHTML = '<img id="captured_image" src="'+data_uri+'">'
+    });
+}
+
+console.log("ml5 version :",ml5.version);
+
+classifier = ml5.imageClassifier("model.json",modleLoaded);
